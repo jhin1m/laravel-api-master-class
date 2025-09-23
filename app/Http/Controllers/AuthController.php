1f689653752672fbc610ab9e6c87a6391062f8b4
jhin1m <ducanhfake@gmail.com>
2025-09-23 19:19:36 +0700

    Implement authentication with Laravel Sanctum and create API routes

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Traits\ApiResponses;
+class AuthController extends Controller
+{
+    use ApiResponses;
+    public function login(){
+        return $this->ok('Login successful');
+    }
+}
