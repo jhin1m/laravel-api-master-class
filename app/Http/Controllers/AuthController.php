ea23f3d8685f55ada69778dee24c92618ecd8b11
jhin1m <ducanhfake@gmail.com>
2025-09-24 10:54:42 +0700

    Implement login functionality with validation and update API routes

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Traits\ApiResponses;
+use App\Http\Requests\ApiLoginRequest;
 class AuthController extends Controller
 {
     use ApiResponses;
-    public function login(){
-        return $this->ok('Login successful');
+
+    public function login(ApiLoginRequest $request)
+    {
+        // Handle login logic here
+        return $this->ok($request->get('email'));
+    }
+
+    public function register(){
+        return $this->ok('register');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
-Route::get('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/register', [AuthController::class, 'register']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
