99cc0647ce2b704219d1edf417749530c315ffaa
jhin1m <ducanhfake@gmail.com>
2025-09-28 19:00:30 +0700

    Implement authentication functionality with login and register endpoints, and update API routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -5,5 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\TicketController;
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('tickets', TicketController::class);
+    Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
 });
